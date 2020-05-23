5434d6e88617bcbc51d1108e0caddb5769deea22
Jonny Garius <gariusjonathan@hotmail.co.uk>
2020-05-23 21:33:42 +0100

    added walls

--- scripts/app.js
+++ scripts/app.js
@@ -1,81 +1,70 @@
 function main() {
 
   const grid = document.querySelector('#grid')
-  const gridSize = 15
-  let ghost = 16
-  let pacman = 192
-  const wall = [22,37,52,67,202,187,172,157]
   const cells = []
+  const gridSize = 18
+  let blinky = 22
+  let pinky = 169
+  let pacman = 192
+  const wall = [
+    26, 27, 38, 39, 40, 41, 42, 44, 45, 47, 48, 49, 50, 51, 56, 57, 58, 59, 60, 62, 63, 65, 66, 67, 68, 69, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 114, 119, 127, 128, 129, 130, 132, 134, 135, 137, 139, 140, 141, 142, 152, 153, 163, 164, 165, 166, 168, 173, 175, 176, 177, 178, 181, 182, 183, 184, 186, 187, 188, 189, 190, 191, 193, 194, 195, 196, 206, 207, 218, 219, 220, 222, 224, 225, 227, 229, 230, 231, 236, 237, 238, 240, 245, 247, 248, 249, 258, 259, 260, 261, 262, 263, 272, 273, 274, 275, 276, 277, 278, 279, 280, 281, 282, 283, 284, 285
+  ]
 
-  for (let i = 0; i < gridSize ** 2; i++) {
+  function createBoard(gridSize, blinky, pacmanPosition, wall) {
+    for (let i = 0; i < gridSize ** 2; i++) {
 
-    const cell = document.createElement('div')
-
-    if (i === ghost) {
-      cell.classList.add('ghost')
+      const cell = document.createElement('div')
+  
+      if (i === blinky) {
+        cell.classList.add('blinky')
+      }
+      if (i === pacmanPosition) {
+        cell.classList.add('pacman')
+      }
+      if (wall.includes(i)) {
+        cell.classList.add('wall')
+      }
+      if (i <= gridSize - 1 || i % gridSize === 0 || i %  gridSize - gridSize + 1 === 0 || i >= gridSize ** 2 - gridSize) {
+        cell.classList.add('wall')
+      }
+      cell.innerHTML = i
+      cells.push(cell)
+      grid.append(cell)
+  
     }
-    if (i === pacman) {
-      cell.classList.add('pacman')
-    }
-    if (wall.includes(i)) {
-      cell.classList.add('wall')
-    }
-    if (i <= 14 || i % gridSize === 0 || i %  gridSize - gridSize + 1 === 0 || i >= 210) {
-      cell.classList.add('wall')
-    }
-    if (i === 105 || i === 119) {
-      cell.classList.remove('wall')
-    }
-    cell.innerHTML = i
-    cells.push(cell)
-    grid.append(cell)
 
   }
 
-  const test = cellsToVectors(cells, gridSize)
-
-  const route = BFS(test, gridSize)
-
-  const interval2 = setInterval(() => {
-    if (ghost === pacman) {
-      clearInterval(interval2)
-      console.log('woo')
-    } else {
-      const test2 = cellsToVectors(cells, gridSize)
-      console.log(test2)
-      console.log(BFS(test2, gridSize))
-    }
-  }, 3000)
-  
-  let i = 0
+  createBoard(18, blinky, pacman, wall)
 
   const interval1 = setInterval(() => {
-    if (ghost === pacman) {
+    if (blinky === pacman) {
       clearInterval(interval1)
       cells[pacman].classList.remove('pacman')
-      cells[ghost].classList.add('ghost')
+      cells[blinky].classList.add('blinky')
     } else {
-      cells[ghost].classList.remove('ghost')
-      ghost += route[i]
-      i++
-      cells[ghost].classList.add('ghost')
+      const blinkyMap = cellsToVectors(cells, gridSize)
+      const blinkyRoute = BFS(blinkyMap, gridSize)
+      cells[blinky].classList.remove('blinky')
+      blinky += blinkyRoute[0]
+      cells[blinky].classList.add('blinky')
     }
-  }, 1000)
+  }, 500)
 
   window.addEventListener('keydown', (e) => {
-    if (e.keyCode === 37) {
+    if (e.keyCode === 65) {
       cells[pacman].classList.remove('pacman')
       pacman--
       cells[pacman].classList.add('pacman')
-    } else if (e.keyCode === 38) {
+    } else if (e.keyCode === 87) {
       cells[pacman].classList.remove('pacman')
       pacman -= gridSize
       cells[pacman].classList.add('pacman')
-    } else if (e.keyCode === 39) {
+    } else if (e.keyCode === 68) {
       cells[pacman].classList.remove('pacman')
       pacman++
       cells[pacman].classList.add('pacman')
-    } else if (e.keyCode === 40) {
+    } else if (e.keyCode === 83) {
       cells[pacman].classList.remove('pacman')
       pacman += gridSize
       cells[pacman].classList.add('pacman')
@@ -100,8 +89,7 @@ class Vector {
   }
 }
 
-// Converts current game state (stored in cells) from an array into an array of arrays. Once split, it then recreates the board with vectors 
-// putting the new vector grid, ghost and pacman into an object which is fed into BFS.
+// Converts current game state (stored in cells) from an array into an array of arrays. Once split, it then recreates the board with vectors putting the new vector grid, ghost and pacman into an object which is fed into BFS algorithm
 function cellsToVectors(gameGridState, gridSize) {
 
   // split gameState into array or array (cells)
@@ -117,7 +105,7 @@ function cellsToVectors(gameGridState, gridSize) {
     const temp = []
     for (let x = 0; x < gridSize; x++) {
       const v = new Vector(y, x, updatedState[y][x].className)
-      if (v.value === 'ghost') {
+      if (v.value === 'blinky' || v.value === 'pinky' || v.value === 'inky' || v.value === 'clyde') {
         ghost = v
       } else if (v.value === 'pacman') {
         pacman = v
