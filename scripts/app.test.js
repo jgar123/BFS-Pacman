d2079dbb24548f794aa69dcfbd8f46fb463d9cfc
Jonny Garius <gariusjonathan@hotmail.co.uk>
2020-05-24 13:48:20 +0100

    split out

--- scripts/app.test.js
+++
@@ -1,3 +0,0 @@
-/* global describe, test, expect */
-
-// const { neighbourCells, BFS, routeMap } = require('./app')
---
+++ scripts/bfs.test.js
@@ -0,0 +1,10 @@
+/* global describe, test, expect */
+
+const bfs = require('./bfs')
+
+console.log(bfs)
+describe('blah', () => {
+  test('blah', () => {
+    expect('blah').toBe('blah')
+  })
+})
